bf00094f5bd4c61aaa7af07c9287b366b5b497ab
mincheol <lmch0000@gmail.com>
2022-09-15 11:14:56 +0900

    update to helm apiserver 0.0.2

--- install.sh
+++ install.sh
@@ -12,21 +12,23 @@ else
 fi
 
 if [[ -z ${helmApiServerVersion} ]]; then
-    helmApiServerVersion=0.0.0
+    helmApiServerVersion=0.0.2
 else
     helmApiServerVersion=${helmApiServerVersion}
 fi
 
-echo "================== get console host =================="
-export consoleHost=$(kubectl get ingressroute -n api-gateway-system console -o jsonpath='{@.spec.routes[0].match}' | awk '{print $6}' | cut -d'`' -f2 | sed 's/console/helm/g')
+#echo "================== get console host =================="
+#export consoleHost=$(kubectl get ingressroute -n api-gateway-system console -o jsonpath='{@.spec.routes[0].match}' | awk '{print $6}' | cut -d'`' -f2 | sed 's/console/helm/g')
 
 # change parameters
 sed -i "s|{imageRegistry}|${imageRegistry}|g" ${yaml_dir}/deploy.yaml
 sed -i "s|{helmApiServerVersion}|${helmApiServerVersion}|g" ${yaml_dir}/deploy.yaml
 sed -i "s|{consoleHost}|${consoleHost}|g" ${yaml_dir}/ingress.yaml
-sed -i "s|{scName}|${scName}|g" ${yaml_dir}/pvc.yaml
+sed -i "s|{storageClass}|${storageClass}|g" ${yaml_dir}/pvc.yaml
 
 # install
+kubectl apply -f ${yaml_dir}/certificate.yaml
 kubectl apply -f ${yaml_dir}/pvc.yaml 
 kubectl apply -f ${yaml_dir}/deploy.yaml
 kubectl apply -f ${yaml_dir}/ingress.yaml
+
